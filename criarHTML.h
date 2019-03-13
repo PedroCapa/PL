3ba470cc24a65296b36fda876275debcf5251c8b
LFCC98 <a81960@alunos.uminho.pt>
2019-03-13 19:38:11 +0000

    Removi uns files vazios

--- criarHTML.h
+++
@@ -1,7 +0,0 @@
-#include <glib.h>
-
-void limpa(GTree * tree, char * info, char * categoria, 
-					char * titulo, char * data, char * texto, char * tags);
-
-void escreveFile(GTree * tree, char * info, char * categoria, 
-					char * titulo, char * data, char * texto, char * tags);
